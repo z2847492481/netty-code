cf098492e7f0efebd43b683eed41ae2ebded292c
zhq123 <2847492481@qq.com>
2024-03-23 19:32:30 +0800

    init

---
+++ src/main/java/com/zhq/HelloTest.java
@@ -0,0 +1,14 @@
+package com.zhq;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @DATE: 2024-03-23 19:25
+ * @Author: zhq123
+ */
+@Slf4j
+public class HelloTest {
+    public static void main(String[] args) {
+        log.info("hello world");
+    }
+}
